9562d10b05974f6d6b668c7fc8242e2820314dc4
AlexLongbeard <AGLBRDK.KR@gmail.com>
2023-03-23 18:45:04 +0300

    done_4

--- src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
+++ src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
@@ -2,7 +2,7 @@ package com.epam.mjc.stage0;
 
 import com.epam.mjc.stage0.utils.Fridge;
 
-public class FridgeAlgorithm {
+public class FridgeAlgorithm implements Fridge {
 
     /**
      * This is a first task in this module. You need to get milk from a fridge =)
@@ -11,6 +11,13 @@ public class FridgeAlgorithm {
      * P.S All methods of interaction with the fridge can be found inside the {@see Fridge} class.
      */
     public void fridgeAlgorithm(Fridge fridge) {
-
+        Fridge.open(fridge);
+        Fridge.getMilk(fridge);
+        Fridge.close(fridge);
     }
-}
+    }
+
+
+
+
+
